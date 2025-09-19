76ce73bfa8aa8433023bf5f5168d3cbde0790ada
Utkarsh chaudhary <155638059+Utkarshchoudhary2@users.noreply.github.com>
2025-09-19 16:45:05 +0530

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -2,3 +2,7 @@
 function toggleSidebar() {
   document.getElementById("sidebar").classList.toggle("expanded");
 }
+// particles.js init
+particlesJS.load('particles-js', 'js/particles.json', function() {
+  console.log('particles.js loaded');
+});
