43c39cb08a5c57282c56ed7d886e79cd775422f1
Utkarsh chaudhary <155638059+Utkarshchoudhary2@users.noreply.github.com>
2025-09-19 17:16:01 +0530

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,70 +1,7 @@
 
-document.addEventListener("DOMContentLoaded", function () {
-  const sidebar = document.querySelector(".sidebar");
-  const logo = document.querySelector(".logo");
-
-  logo.addEventListener("click", () => {
-    sidebar.classList.toggle("expanded");
-  });
-
-  // Particles
-  const canvas = document.getElementById("particles");
-  const ctx = canvas.getContext("2d");
-  let particles = [];
-
-  function resize() {
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
-  }
-  window.addEventListener("resize", resize);
-  resize();
-
-  function Particle() {
-    this.x = Math.random() * canvas.width;
-    this.y = Math.random() * canvas.height;
-    this.vx = (Math.random() - 0.5) * 1;
-    this.vy = (Math.random() - 0.5) * 1;
-    this.radius = 2;
-  }
-  Particle.prototype.update = function () {
-    this.x += this.vx;
-    this.y += this.vy;
-    if (this.x < 0 || this.x > canvas.width) this.vx *= -1;
-    if (this.y < 0 || this.y > canvas.height) this.vy *= -1;
-  };
-  Particle.prototype.draw = function () {
-    ctx.beginPath();
-    ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
-    ctx.fillStyle = "white";
-    ctx.fill();
-  };
-
-  function init() {
-    particles = [];
-    for (let i = 0; i < 100; i++) {
-      particles.push(new Particle());
-    }
-  }
-  function animate() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    for (let i = 0; i < particles.length; i++) {
-      particles[i].update();
-      particles[i].draw();
-      for (let j = i + 1; j < particles.length; j++) {
-        let dx = particles[i].x - particles[j].x;
-        let dy = particles[i].y - particles[j].y;
-        let dist = Math.sqrt(dx * dx + dy * dy);
-        if (dist < 100) {
-          ctx.beginPath();
-          ctx.moveTo(particles[i].x, particles[i].y);
-          ctx.lineTo(particles[j].x, particles[j].y);
-          ctx.strokeStyle = "rgba(255,255,255,0.1)";
-          ctx.stroke();
-        }
-      }
-    }
-    requestAnimationFrame(animate);
-  }
-  init();
-  animate();
+function toggleSidebar() {
+  document.getElementById("sidebar").classList.toggle("expanded");
+}
+particlesJS.load('particles-js', 'js/particles.json', function() {
+  console.log('particles.js loaded');
 });
