c61a20d83f1fa573f7e60dfe61502ae692d5ab4c
Utkarsh chaudhary <155638059+Utkarshchoudhary2@users.noreply.github.com>
2025-09-22 17:27:34 +0530

    Update firebase-config.js

--- assets/firebase-config.js
+++ assets/firebase-config.js
@@ -1,10 +1,11 @@
 // firebase-config.js
-// TODO: Replace with your Firebase project configuration
-export const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT_ID.appspot.com",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID"
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyB1NtsmihmojQsWzOwqRo-286cv12spgdE",
+  authDomain: "utkarshchaudhary-portfolio.firebaseapp.com",
+  projectId: "utkarshchaudhary-portfolio",
+  storageBucket: "utkarshchaudhary-portfolio.firebasestorage.app",
+  messagingSenderId: "487685457505",
+  appId: "1:487685457505:web:797c7ac6cc2e40d8c1cc20",
+  measurementId: "G-VYF9PRY0F9"
 };
